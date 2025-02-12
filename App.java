907bbd3164fe9cf9591e59453bd844bf86d883e5
Xande <Alexandre.froes@estudante.iftm.edu.br>
2025-02-12 15:04:54 -0300

    Envio completo da atividade de poo1

--- App.java
+++ App.java
@@ -1,4 +1,3 @@
-import java.util.HashSet;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,46 +7,30 @@ public class App {
         """
         1 - adicionar produto
         2 - adicionar categoria
-        3 - listar produtos
-        4 - montar carrinho 
-        5 - sair
+        3 - listar produtos no estoque
+        4 - aumentar estoque 
+        5 - diminuir estoque
+        6 - sair
         """;
 
-    public static final String MENUCARRINHO =
-        """
-        1 - adicionar item no carrinho
-        2 - remover item do carrinho
-        3 - valor total do carrinho
-        4 - adicionar desconto 
-        5 - sair
-        """;
 
-    public static void adicionarCategorias(Scanner ler){
-        Categoria.addCategorias("Eletronico");
-        Categoria.addCategorias("Alimento");
-        Categoria.addCategorias("Brinquedo");
-
-        do{
+    public static void adicionarCategorias(Scanner ler) {    
+        do {
             System.out.println("Qual categoria deseja adicionar?");
+            String novaCat = ler.next();
+            adicionarCategoria(novaCat);
             
-            if(!Categoria.addCategorias(ler.next())){
-                System.out.println("Categoria já existe");
-            
-            }else{
-                System.out.println("Categoria adicionada com sucesso!!");
-            }
-            
-            System.out.println("Deseja adicionar mais categorias? (s/n)");
-            String opcao = ler.next();
-
-            if(opcao.equalsIgnoreCase("s")){
-                continue;
-            
-            }else{
-                break;
-            }
-
-        }while(true);
+            System.out.println("Deseja adicionar mais? (s/n)");
+            if(!ler.next().equalsIgnoreCase("s")) break;
+        } while(true);
+    }
+    
+    public static void adicionarCategoria(String nomeCategoria) {
+        if(!Categoria.addCategorias(nomeCategoria)) {
+            System.out.println("Categoria " + nomeCategoria + " já existe");
+        } else {
+            System.out.println("Categoria " + nomeCategoria + " adicionada!");
+        }
     }
 
     public static void adicionarProduto(Scanner ler) {
@@ -61,7 +44,7 @@ public class App {
                 System.out.println("Digite o preço do produto: ");
                 preco = ler.nextDouble();
                 ler.nextLine();
-                Produto.validarPrecoEstoque(preco);
+                Estoque.validarPrecoEstoque(preco);
                 precoValido = true;
             } catch (InputMismatchException e) {
                 System.out.println("Preço inválido");
@@ -78,7 +61,8 @@ public class App {
                 System.out.println("Digite a quantidade do produto: ");
                 quantidade = ler.nextInt();
                 ler.nextLine();
-                Produto.validarEstoque(quantidade);
+                Estoque.validarEstoque(quantidade);
+
                 quantidadeValida = true;
             } catch (InputMismatchException e) {
                 System.out.println("Quantidade inválida");
@@ -102,12 +86,18 @@ public class App {
 
                 if(cadastro.equalsIgnoreCase("s")){
                     System.out.println("Digite o nome da categoria: ");
-                    categoria = ler.next();
-                    Categoria.addCategorias(categoria);
+                    String novaCat = ler.next();
+                    adicionarCategoria(novaCat);
+                    Categoria cat = new Categoria(novaCat);
 
-                    Categoria cat = new Categoria(categoria);
-                    Produto produto = new Produto(nome, preco, quantidade, cat);
-                    Produto.addProduto(produto);
+                    try {
+                        Produto produto = Estoque.criarProduto(nome, preco, quantidade, cat);
+                        System.out.println("\nProduto cadastrado com sucesso!!");
+                        Estoque.addProduto(produto);
+                    } catch (EstoqueException e) {
+                        System.out.println("Erro ao criar produto: " + e.getMessage());
+                    }
+
                     System.out.println("\nProduto e categoria cadastrados com sucesso!!!");
                 }else{
                     System.out.println("Cadastro cancelado");
@@ -129,93 +119,63 @@ public class App {
         }while(!categoriaValida);
 
         Categoria cat = new Categoria(categoria);
-        Produto produto = new Produto(nome, preco, quantidade, cat);
-        Produto.addProduto(produto);
+        try {
+            Produto produto = Estoque.criarProduto(nome, preco, quantidade, cat);
+            System.out.println("\nProduto cadastrado com sucesso!!");
+            Estoque.addProduto(produto);
+        } catch (EstoqueException e) {
+            System.out.println("Erro ao criar produto: " + e.getMessage());
+        }
         System.out.println("\nProduto cadastrado com sucesso!!");
     }
 
-    public static void adicionarItem(Scanner ler) {
-        Carrinho carrinho = new Carrinho();
-        Produto produto = new Produto();
-        HashSet<Produto> produtos = Produto.getListaProdutos();
-
-        System.out.println("Produtos:");
-        Produto.listarProdutosQuantidade();
-
-        boolean nomeValido = false;
+    public static void aumentarEstoque(Scanner ler) {
+        boolean produtoExiste = false;
         do {
-            System.out.println("Digite o nome do produto que deseja adicionar: "); 
+            Estoque.listarProdutos();
+            System.out.println("Digite o nome do produto:");
             String nome = ler.nextLine();
-            for (Produto p : produtos) {
-                if (p.getNome().equalsIgnoreCase(nome)) {
-                    produto = p;
-                    nomeValido = true;
-                    break;
-                } else {
-                    System.out.println("Produto não encontrado. Digite novamente");
-                    continue;
+            Produto p = Estoque.buscarProduto(nome);
+        
+            if (p == null) {
+                System.out.println("Produto não encontrado");
+            } else {
+                System.out.println("Digite a quantidade que deseja adicionar:");
+                int quantidade = ler.nextInt();
+                ler.nextLine();
+        
+                Estoque.aumentarEstoque(p, quantidade);
+                System.out.println("Estoque atualizado, nova quantidade de " + p.getNome() + ": " + p.getEstoque());
+                produtoExiste = true;
+            }
+        } while (!produtoExiste);
+    }
+
+    public static void diminuirEstoque(Scanner ler) {
+        boolean produtoExiste = false;
+        do {
+            Estoque.listarProdutos();
+            System.out.println("Digite o nome do produto:");
+            String nome = ler.nextLine();
+            Produto p = Estoque.buscarProduto(nome);
+        
+            if (p == null) {
+                System.out.println("Produto não encontrado");
+            } else {
+                System.out.println("Digite a quantidade que deseja diminuir do estoque:");
+                int quantidade = ler.nextInt();
+                ler.nextLine();
+                
+                try {
+                    Estoque.diminuirEstoque(p, quantidade);
+                    System.out.println("Estoque atualizado, nova quantidade de " + p.getNome() + ": " + p.getEstoque());
+                    produtoExiste = true;
+                } catch (EstoqueException e) {
+                    System.out.println(e.getMessage());
                 }
             }
-        } while (!nomeValido);
-
-        boolean quantidadeValida = false;
-        int quantidade = 0;
-        do {
-            try {
-                System.out.println("Digite a quantidade do produto que deseja adicionar: ");
-                quantidade = ler.nextInt();
-                ler.nextLine();
-                carrinho.adicionarItem(produto, quantidade);
-                produto.diminuirEstoque(quantidade);
-                quantidadeValida = true;
-                System.out.println("Produto adicionado ao carrinho com sucesso");
-                System.out.println("Estoque atualizado: " + produto.getNome() + " x" + produto.getEstoque());
-            } catch (InputMismatchException e) {
-                System.out.println("Quantidade inválida");
-                ler.nextLine();
-            } catch (EstoqueException e) {
-                System.out.println(e.getMessage());
-            }
-        } while (!quantidadeValida);
+        } while (!produtoExiste);
     }
-<<<<<<< HEAD
-    
-=======
-
-    public static void montarCarrinho(Scanner ler) {
-        int opcao = 0;
-
-        do {
-            System.out.println(MENUCARRINHO);
-            opcao = ler.nextInt();
-            ler.nextLine();
-
-            switch (opcao) {
-                case 1:
-                    adicionarItem(ler);
-                    break;
-                case 2:
-                    // Carrinho.listarCarrinho();
-                    // System.out.println("Digite o nome do produto que deseja remover: ");
-
-                    break;
-                case 3:
-                    
-                    break;
-                case 4:
-                    
-                    break;
-                case 5:
-                    System.out.println("Voltando pro menu principal");
-                    break;
-                default:
-                    System.out.println("Opção inválida");
-                    break;
-            }
-        } while (opcao != 5);
-    }
-
->>>>>>> 32b6c4cae25ae64fdc286e0789e4db314341f05f
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
 
@@ -233,19 +193,22 @@ public class App {
                     adicionarCategorias(ler);
                     break;
                 case 3:
-                    Produto.listarProdutos();
+                    Estoque.listarProdutos();
                     break;
-                case 4:    
-                    montarCarrinho(ler);
+                case 4: 
+                    aumentarEstoque(ler);
                     break;
                 case 5:
-                    System.out.println("Saindo...");
+                    diminuirEstoque(ler);
+                    break;
+                case 6:
+                    System.out.println("Saindo do sistema!");
                     break;
                 default:
                     System.out.println("Opção inválida");
                     break;
             }
             
-        }while(opcao != 5);
+        }while(opcao != 7);
     }
 }
---
+++ Estoque.java
@@ -0,0 +1,87 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class Estoque {
+    private static HashSet<Produto> listaProdutos = new LinkedHashSet<>();
+
+    public static Produto criarProduto(String nome, double preco, int estoqueInicial, Categoria categoria) 
+        throws EstoqueException {
+        
+        validarPrecoEstoque(preco);
+        validarEstoque(estoqueInicial);
+        
+        Produto novoProduto = new Produto(nome, preco, estoqueInicial, categoria);
+        if(!listaProdutos.add(novoProduto)) {
+            throw new EstoqueException("Produto já existe no estoque");
+        }
+        return novoProduto;
+    }
+
+    public static void addProduto(Produto produto) {
+        listaProdutos.add(produto);
+    }
+
+    public static void removerProduto(Produto produto) {
+        listaProdutos.remove(produto);
+    }
+    
+    public static void listarProdutos(){
+        for(Produto p : listaProdutos){
+            System.out.println("- " + p.getNome() + 
+                            " x" + p.getEstoque() + " unidades" + 
+                            " R$ " + p.getPreco() + 
+                            " Categoria: " + p.getCategoria().getNome());
+        }
+        System.out.println();
+    }
+
+    public static void diminuirEstoque(Produto produto, int quantidade) throws EstoqueException {
+        if (quantidade <= 0) {
+            throw new EstoqueException("Quantidade deve ser maior que zero.");
+        } else if (quantidade > produto.getEstoque()) {
+            throw new EstoqueException("Estoque insuficiente para " + produto.getNome() + " escolha uma quantidade menor que " + produto.getEstoque());
+        } else {
+            int novoEstoque = produto.getEstoque() - quantidade;
+            produto.setEstoque(novoEstoque);
+        }
+    }
+
+    public static void aumentarEstoque(Produto produto, int quantidade) {
+        int novoEstoque = produto.getEstoque() + quantidade;
+        produto.setEstoque(novoEstoque);
+    }
+    
+    public static void aumentarEstoque(Produto produto) {
+        aumentarEstoque(produto, 1);
+    }
+
+    
+    public static void validarPrecoEstoque(double preco) throws EstoqueException {
+        if (preco <= 0) {
+            throw new EstoqueException("Preço precisa ser maior que 0");
+        }
+    }
+
+    public static void validarEstoque(int estoque) throws EstoqueException {
+        if (estoque <= 0) {
+            throw new EstoqueException("produto.getEstoque() precisa ser maior que 0");
+        }
+    }
+
+    public static Produto buscarProduto(String nome) {
+        for (Produto p : listaProdutos) {
+            if (p.getNome().equalsIgnoreCase(nome)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public static HashSet<Produto> getListaProdutos() {
+        return listaProdutos;
+    }
+
+    public static void setListaProdutos(HashSet<Produto> listaProdutos) {
+        Estoque.listaProdutos = listaProdutos;
+    }
+}
